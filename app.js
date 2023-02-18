db4b9660f7b38b33a47c149366ea638a3cb265fa
AetherIna <facundo2474@hotmail.com>
2023-02-18 16:12:54 -0300

    changes

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const path = require('path');
 
 const port = 3030;
 
-app.get ('/', (res, req) => res.sendFile(path.resolve('./views/home.html')));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(path.resolve('./views/index.html')));
+app.get('/', (req, res) => res.sendFile(path.resolve('./views/login.html')));
+app.get('/', (req, res) => res.sendFile(path.resolve('./views/register.html')));
 
 app.listen(port, () => console.log(`El servidor funcionando en el puerto ${port}`));
 
